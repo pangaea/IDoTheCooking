6d5acea75db69c76da74bd70918e526a5c26e898
Kevin Jacovelli <kjac@optonline.net>
2025-05-02 01:10:10 -0400

    Version 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pangaea.idothecooking"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "6.0"
 
         // Load the values from apikey.properties file
         val keystoreFile = project.rootProject.file("apikey.properties")
