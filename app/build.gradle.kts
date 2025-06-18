3342400b700b0d3d706aa5063a398eeb49136698
Kevin Jacovelli <kjac@optonline.net>
2025-06-18 00:19:45 -0400

    Started version 12 and upgrade to meekat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pangaea.idothecooking"
         minSdk = 21
         targetSdk = 34
-        versionCode = 11
-        versionName = "11.0"
+        versionCode = 12
+        versionName = "12.0"
 
         // Load the values from apikey.properties file
         val keystoreFile = project.rootProject.file("apikey.properties")
