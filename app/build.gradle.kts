016528a408e04a74bd049a069f68aa1916887a55
Kevin Jacovelli <kjac@optonline.net>
2025-07-22 12:13:14 -0400

    Version 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pangaea.idothecooking"
         minSdk = 21
         targetSdk = 35
-        versionCode = 12
-        versionName = "12.0"
+        versionCode = 13
+        versionName = "13.0"
 
         // Load the values from apikey.properties file
         val keystoreFile = project.rootProject.file("apikey.properties")
